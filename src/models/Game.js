696c0e2984f20490b61315a5b54050e7a29ab2d5
khang <haphongpk12@gmail.com>
2024-05-13 17:07:36 +0700

    feat: update game model

--- src/models/Game.js
+++ src/models/Game.js
@@ -2,38 +2,52 @@ const { Sequelize, DataTypes } = require("sequelize");
 const Connection = require("../database");
 
 const Game = Connection.define("games", {
-    title: {
-        type: DataTypes.STRING(30),
-        allowNull: false,
-        unique: true,
-        validate: {
-            isLowercase: true,
-            notNull: true,
-            notEmpty: true,
-        }
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+    unique: true,
+    validate: {
+      isLowercase: true,
+      notNull: true,
+      notEmpty: true,
     },
-    price: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-        validate: {
-            isInt: true,
-            notNull: true,
-        }
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    validate: {
+      isInt: true,
+      notNull: true,
     },
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        validate: {
-            isInt: true,
-            notNull: true,
-            notEmpty: true,
-        }
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    validate: {
+      isInt: true,
+      notNull: true,
+      notEmpty: true,
     },
-    slug: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-
+  },
+  slug: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.literal(
+      "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+    ),
+  },
 });
 
 module.exports = Game;
